21cf9056f9ca35df6def69337f58028bf8e5796a
SharlShaboyan1 <141414247+SharlShaboyan1@users.noreply.github.com>
2024-08-01 17:20:36 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
             <img src={viteLogo} className="logo" alt="Vite logo" />
           </a>
         </div>
-        <h1>Vite + React + TS + KUBERNETESS</h1>
+        <h1>Vite + React + TS + KUBERNETESS + +</h1>
         <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
